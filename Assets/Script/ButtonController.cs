118b838344d1abe1e3d9a32ebe51e13eb3d27e8c
John5491 <animatedar2022@gmail.com>
2022-05-01 13:47:52 +0800

    Import Models

--- Assets/Script/ButtonController.cs
+++ Assets/Script/ButtonController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
     public void Execute_LearningMode()
     {
         Debug.Log("Begining Leaning Mode...");
+        SceneManager.LoadScene(1);
     }
 
     public void Execute_Tutorial()
@@ -18,5 +20,11 @@ public class ButtonController : MonoBehaviour
     {
         Debug.Log("Exiting...");
         Application.Quit();
+    }
+
+    public void Execute_Back(int sceneBuildIndex)
+    {
+        Debug.Log("Returning to scene " + sceneBuildIndex + "...");
+        SceneManager.LoadScene(sceneBuildIndex);
     }
 }
